10b947243592b102f34b0167c2ea55da0e8dbe2d
Piu120802 <165129529+Piu120802@users.noreply.github.com>
2024-03-27 15:21:53 +0530

    Update independanceDayChecker.java

--- independanceDayChecker.java
+++ independanceDayChecker.java
@@ -1,3 +1,4 @@
+/*1. Create a Java program that acts as a simple calculator. ● The program should prompt the user to enter two numbers and an operator (+, -, *, /). ● Perform the corresponding calculation based on the operator. ● Handle potential exceptions, such as division by zero or invalid operator input. ● Display the result or an appropriate error message.*/
 package Anudipp;
 import java.time.LocalDate;
 import java.time.MonthDay;
