50df4436ba169a649c1b1f3959c448c891abc386
Piu120802 <165129529+Piu120802@users.noreply.github.com>
2024-03-27 15:23:38 +0530

    Update simpleBankingApp.java

--- simpleBankingApp.java
+++ simpleBankingApp.java
@@ -1,3 +1,4 @@
+/* 2. Write a Java program to simulate a simple banking application. ● Create a class BankAccount with a balance and methods for deposit and withdrawal. ● Implement exception handling for withdrawal operations to prevent overdrawing. ● Handle the scenario when the withdrawal amount is greater than the balance.*/
 package Anudipp;
 	import java.util.Scanner;
 	class BankAccount 
